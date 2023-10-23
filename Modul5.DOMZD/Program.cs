c8a30fe3c9eec5a787634db5b9cb688109ff1ea1
Aru2003 <baymolda03@inbox.ru>
2023-10-23 11:06:03 +0600

    Добавьте файлы проекта.

---
+++ Modul5.DOMZD/Program.cs
@@ -0,0 +1,22 @@
+﻿//Перехват исключения запроса к несуществующему веб ресурсу:
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Попытка выполнить запрос к несуществующему веб-ресурсу.
+            // Здесь может возникнуть исключение, например, System.Net.WebException.
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла ошибка: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Завершение программы.");
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Обращение к элементам массива и выход за его пределы:
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 3 };
+
+        try
+        {
+            // Попытка обратиться к элементу, выходящему за пределы массива.
+            int value = arr[5];
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Произошла ошибка: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Завершение обработки массива.");
+        }
+    }
+}
