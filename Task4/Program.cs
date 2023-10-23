88fa1c6cfc1c180449e49bcddb780e2af9e2ce19
Aru2003 <baymolda03@inbox.ru>
2023-10-23 11:05:55 +0600

    modul5 задачи генерация исключения в вызываемом методе и его обработка в вызывающем методе:

---
+++ Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿//Генерация исключения в вызываемом методе и его обработка в вызывающем методе:
+using System;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Вызов метода, который генерирует исключение.
+            GenerateException();
+        }
+        catch (CustomException ex)
+        {
+            Console.WriteLine("Произошла пользовательская ошибка: " + ex.Message);
+        }
+    }
+
+    static void GenerateException()
+    {
+        // Генерация пользовательского исключения и его "поднятие" в вызывающий метод.
+        throw new CustomException("Это пользовательское исключение.");
+    }
+}
+
+class CustomException : Exception
+{
+    public CustomException(string message) : base(message)
+    {
+    }
+}
